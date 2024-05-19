ac6874d7b128a6aa76a395004a28ee8231e71db7
Marcelo Filho <marcelo.rodriguespires@gmail.com>
2024-05-19 13:49:15 -0300

    Added tests to uuid package

--- src/external/service/order_service_test.go
+++ src/external/service/order_service_test.go
@@ -2,9 +2,10 @@ package service
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"github.com/ViniAlvesMartins/tech-challenge-fiap-payment/infra"
-	response_order_service "github.com/ViniAlvesMartins/tech-challenge-fiap-payment/src/application/modules/response/order_service"
+	responseorderservice "github.com/ViniAlvesMartins/tech-challenge-fiap-payment/src/application/modules/response/order_service"
 	"github.com/ViniAlvesMartins/tech-challenge-fiap-payment/src/entities/entity"
 	"github.com/ViniAlvesMartins/tech-challenge-fiap-payment/src/entities/enum"
 	"github.com/go-resty/resty/v2"
@@ -21,9 +22,8 @@ func TestOrderService_GetById(t *testing.T) {
 		config, _ := infra.NewConfig()
 		client := resty.New().SetBaseURL(config.OrdersURL)
 		logger := slog.New(slog.NewTextHandler(os.Stderr, nil))
-		//url := fmt.Sprintf("%s/%s", config.OrdersURL, "orders/1")
 
-		response := response_order_service.GetByIdResp{
+		response := responseorderservice.GetByIdResp{
 			Error: "",
 			Data: &entity.Order{
 				ID:          1,
@@ -48,5 +48,59 @@ func TestOrderService_GetById(t *testing.T) {
 
 		assert.Equal(t, expectedJsonResponse, jsonResponse)
 		assert.Nil(t, err)
+	})
+
+	t.Run("error making request", func(t *testing.T) {
+		config, _ := infra.NewConfig()
+		client := resty.New()
+		logger := slog.New(slog.NewTextHandler(os.Stderr, nil))
+		expectedErr := errors.New("Get \"orders\": no responder found")
+
+		response := responseorderservice.GetByIdResp{
+			Error: "",
+			Data: &entity.Order{
+				ID:          1,
+				ClientId:    nil,
+				StatusOrder: enum.AWAITING_PAYMENT,
+				Amount:      123.45,
+				CreatedAt:   time.Now(),
+			},
+		}
+
+		httpmock.ActivateNonDefault(client.GetClient())
+		defer httpmock.DeactivateAndReset()
+
+		httpmock.RegisterResponder("GET", fmt.Sprintf("%s%s", config.OrdersURL, "/orders/1"),
+			httpmock.NewJsonResponderOrPanic(200, response))
+
+		orderService := NewOrderService(client, logger)
+		order, err := orderService.GetById(1)
+
+		assert.Errorf(t, expectedErr, err.Error())
+		assert.Nil(t, order)
+	})
+
+	t.Run("request with error code response", func(t *testing.T) {
+		config, _ := infra.NewConfig()
+		client := resty.New().SetBaseURL(config.OrdersURL)
+		logger := slog.New(slog.NewTextHandler(os.Stderr, nil))
+		expectedErr := errors.New("request response error")
+
+		response := responseorderservice.GetByIdResp{
+			Error: "Internal server error",
+			Data:  nil,
+		}
+
+		httpmock.ActivateNonDefault(client.GetClient())
+		defer httpmock.DeactivateAndReset()
+
+		httpmock.RegisterResponder("GET", fmt.Sprintf("%s%s", config.OrdersURL, "/orders/1"),
+			httpmock.NewJsonResponderOrPanic(500, response))
+
+		orderService := NewOrderService(client, logger)
+		order, err := orderService.GetById(1)
+
+		assert.Errorf(t, expectedErr, err.Error())
+		assert.Nil(t, order)
 	})
 }
